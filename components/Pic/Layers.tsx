51d6479c3c1322f01751042827e099308dc9f901
Leo <ferteleo@gmail.com>
2023-12-15 15:01:38 +0100

    fix TS error layers component

--- components/Pic/Layers.tsx
+++ components/Pic/Layers.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from "@/components/Pic/Layers.module.css"
 
-export default function Layers(props ) {
+export default function Layers(props: { onClickRight: React.MouseEventHandler<HTMLDivElement> | undefined; onClickLeft: React.MouseEventHandler<HTMLDivElement> | undefined } ) {
   return (
   <>
     <div className={styles.rightLayer} onClick={props.onClickRight}></div>
