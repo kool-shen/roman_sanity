375f2ee01ddf658696a60fec3da506a28c75de45
Leo <ferteleo@gmail.com>
2024-02-10 15:49:18 +0100

    fix ajout album test sans la condition

--- pages/photo/[album].tsx
+++ pages/photo/[album].tsx
@@ -115,13 +115,13 @@ export async function getStaticProps({ params }: { params: { album: string } }) 
 
   const allData = await getAllAlbums(); 
 
-  if (!albumData)   {
-    console.log("nope")
-    return {
-      notFound: true,
+  // if (!albumData)   {
+  //   console.log("nope")
+  //   return {
+  //     notFound: true,
       
-    }
-  }
+  //   }
+  // }
  
   return {
     props: {
